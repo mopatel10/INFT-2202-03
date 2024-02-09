056196da5acda93059ef13f662fd5513e3ac5272
Mohammed Patel <mohammed.patel10@dcmail.ca>
2024-02-08 20:09:28 -0500

    VC commit

---
+++ Lab1/scripts/app.js
@@ -0,0 +1,50 @@
+// Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+// Add Welcome Text
+let welcomeText = "Welcome to my Website";
+document.getElementById("welcomeText").innerHTML = welcomeText;
+
+// Add Welcome Note
+let welcomeNote = "Explore my world of creativity and inspiration. I'm here to provide you with amazing content and experiences.";
+document.getElementById("welcomeNote").innerHTML = welcomeNote;
+});
+
+// Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+    // Add title and description to product 1
+    document.getElementById("gameTitle").innerHTML = "Black Ops II";
+    document.getElementById("gameDesc").innerHTML = "Black Ops II is an amazing game with an amazing multiplayer, an amazing campaign, and amazing zombies. The campaign is one of the best in the series with Mendez being one of the best villains in gaming, along with amazing characters like Frank Woods, Alex Mason, David Mason, and Harper.";
+
+    // Add title and description to product 2
+    document.getElementById("movieTitle").innerHTML = "As Above, So Below";
+    document.getElementById("movieDesc").innerHTML = "As Above, So Below is a film about a group of subterranean explorers who encounter terror beneath the streets of Paris after descending into the city's ancient catacombs. Just when it seems their journey can't get any more nightmarish, the demons of their pasts strike back with a vengeance.";
+
+    // Add title and description to product 3
+    document.getElementById("bookTitle").innerHTML = "Bones";
+    document.getElementById("bookDesc").innerHTML = "Bones is a graphic novel series about the three Bone cousins -- Fone Bone, Phoney Bone, and Smiley Bone -- who are separated and lost in a vast, uncharted desert. One by one, they find their way into a deep, forested valley filled with wonderful and terrifying creatures.";
+});
+
+// Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+    // Service 1
+    let service1Name = "Web Designing";
+    let service1Desc = "Web designing encompasses a range of disciplines, including visual design, user experience (UX) design, and front-end development. Leveraging my expertise in graphic design, HTML, CSS, and JavaScript, I meticulously design and develop websites that not only look great but also function seamlessly across devices and browsers.";
+
+    document.getElementById("service1Name").innerText = service1Name;
+    document.getElementById("service1Desc").innerText = service1Desc;
+
+    // Service 2
+    let service2Name = "Object Oriented Programming";
+    let service2Desc = "Object-Oriented Programming (OOP) is a powerful paradigm used in software development for creating modular, scalable, and reusable code. As an experienced developer proficient in OOP, I leverage this approach to build robust and maintainable software solutions tailored to your specific needs.";
+
+    document.getElementById("service2Name").innerText = service2Name;
+    document.getElementById("service2Desc").innerText = service2Desc;
+
+    // Service 3
+    let service3Name = "Mobile Development";
+    let service3Desc = "C# for Windows Forms development is a specialized skillset focused on creating desktop applications for the Windows operating system using the C# programming language and the Windows Forms framework. As a proficient developer in C# for Windows Forms, I specialize in building intuitive and feature-rich desktop applications tailored to meet your specific business needs.";
+
+    document.getElementById("service3Name").innerText = service3Name;
+    document.getElementById("service3Desc").innerText = service3Desc;
+});
+
