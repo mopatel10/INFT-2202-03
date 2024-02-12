69d8db1c9381dd58dfd9fc8abcd11e62c95869f7
Mohammed Patel <mohammed.patel10@dcmail.ca>
2024-02-12 18:58:29 -0500

    Final commit

--- WEBD6201_ICE4_starter/scripts/app.js
+++ WEBD6201_ICE4_starter/scripts/app.js
@@ -1,5 +1,5 @@
 console.log('app.js loaded')
-
+import { User } from './user.js' ; 
 /**
  * iffe to insert nav bar at the top of each page
  */
@@ -39,7 +39,7 @@ $(function () {
      console.log(contentDiv.text())
 
      //text to use with new paragraph
-     text = "It is one of his best works!"
+     let text = "It is one of his best works!"
      //create a paragraph
      let newP = $("<p></p>");
     // TO DO: add text with html()
@@ -47,10 +47,13 @@ $(function () {
     // append to contentDiv
     contentDiv.append(newP);
     // add text
-    newP.append("I am very excited for the opening of the new adaptation of his work.");
+    newP.append("I am ");
     // TO DO: test when very excited is in a strong tag
-    newP.append("I am <strong>very excited</strong> for the opening of the new adaptation of his work.");
-    // TO DO: append to newP text
+    
+    newP.append("<strong>very excited</strong>");    
+
+    // append to newP text
+    newP.append(" for the opening of the new adaptation of his work.");
     contentDiv.append(newP);
  });
 
@@ -60,16 +63,16 @@ $(function () {
  $(function () {
      //TO DO: complete the function
      //get the button
-    let toggleButton = $("#btnRegSubmit");
+    let toggleButton = $('#toggle');
      //add a click function
     toggleButton.click (function() {
          //get the parent div's p tags
-        let paragraphs = $("div p");
+        let paragraphs = $('div p');
          //for each p in the div
          paragraphs.each(function(index,para) {
              //if it has toggleHide class
-             let currentClass = $(para).attr('class');
-             if ($currentClass == 'toggleHide') {
+            let currentClass = $(para).attr('class');
+            if (currentClass == 'toggleHide') {
                 //remove toggleHide class and add toggleShow class
                 $(para).toggleClass('toggleHide toggleShow');
                 // styling is controlled in the css
@@ -90,7 +93,7 @@ $(function () {
 
 // FORM JQUERY
 // TO DO: import form validation functions with alias
-import * as validators from './form.js';
+import * as validateData from './form.js';
  //if the submit button is on the page
  if ($("#btnRegSubmit")) {
     // TO DO: add a click function that calls a callack function
@@ -110,40 +113,44 @@ import * as validators from './form.js';
             let email = $("#inputEmail").val();
             // get the password input
             let password = $("#inputPassword").val();
-            let confirmPassword = $("#inputPassword2").val();
+            let confirmedPassword = $("#inputPassword2").val();
 
+            let user = new User(firstName, lastName, userName, email, password);
         // debug statement for object
         console.log(`UserDetails: ${user.displayUser()}`)
 
         // validate first name
-        let validateFN = validators.validateFirst(firstName);
+        let validateFN = validateData.validateFirst(firstName);
         // validate last name
-        let validateLN = validators.validateLast(lastName);
+        let validateLN = validateData.validateLast(lastName);
         // validate  username
-        let validateUser = validators.validateUsername(userName);
+        let validateUser = validateData.validateUsername(userName);
         // validate email
-        let validEmail = validators.validateEmail(email);
+        let validateEmail = validateData.validateEmail(email);
         // validate confirm password
-        let validatePass = validators.validatePassword(password,confirmPassword);
+        let validatePass = validateData.validatePassword(password, confirmedPassword);
 
         // Check and print errors for each validated field
         if (validateFN) {
-            console.log(validateFN);
+            $("#first-group .errorMessage").html(validateFN);
+             
         }
         if (validateLN) {
-            console.log(validateLN);
+            $("#last-group .errorMessage").html(validateLN);
+            
         }
         if (validateUser) {
-            console.log(validateUser);
+            $("#username-group .errorMessage").html(validateUser);
+            
         }
         if (validateEmail) {
-            console.log(validEmail);
+            $("#email-group .errorMessage").html(validateEmail);
+            
         }
         if (validatePass) {
-            console.log(validatePass);
+            $("#pass1-group .errorMessage").html(validatePass);
         }
-
- });
+ });    
  }
 
 // TO DO: if reset button present
@@ -167,7 +174,7 @@ if ($("#gallery")) {
 
 function galleryRotate() {
     // get the image tag
-    let imagesClass = $('#images');
+    let imagesId = $('#images');
         // get a list of your images
          let images = [
              "./images/portraits/portrait-01.jpg",
@@ -200,7 +207,7 @@ function galleryRotate() {
         // increment the image index but no greater than how many images you have
         index = (index + 1) % images.length;
         // fade out the current image
-        imagesClass.fadeOut(1000, function() {
+        imagesId.fadeOut(1000, function() {
             // $(this) refers to the object that calls the callback or in this case galleryImage
             // change the src attribute of the image
             $(this).attr("src", images[index]);
@@ -210,5 +217,5 @@ function galleryRotate() {
         // debug statement
         console.log("Switched to image:", images[index]);
         //set the time for more than how long the fade out and in process will take or you won't get the images you expect
-        }, 3000); 
+        }, 2000); 
 }
