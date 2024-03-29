dcef16691b92f3e757624e1f80f72af61a282815
Mohammed Patel <mohammed.patel10@dcmail.ca>
2024-03-29 15:10:31 -0400

    ICE9 added all hbs pages

--- INFT2202-ICE9-main/controllers/userController.js
+++ INFT2202-ICE9-main/controllers/userController.js
@@ -1,3 +1,15 @@
 exports.userView = (req, res) => {
   res.render('home');
 }
+exports.aboutView = (req, res) => {
+  res.render('about');
+}
+exports.pricingView = (req, res) => {
+  res.render('pricing');
+}
+exports.servicesView = (req, res) => {
+  res.render('services');
+}
+exports.productsView = (req, res) => {
+  res.render('products');
+}
--- INFT2202-ICE9-main/routes/user.js
+++ INFT2202-ICE9-main/routes/user.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const usersController = require('../controllers/userController');
 
 router.get('/', usersController.userView);
+router.get('/about', usersController.aboutView);
+router.get('/pricing', usersController.pricingView);
+router.get('/services', usersController.servicesView);
+router.get('/products', usersController.productsView);
 
 module.exports = router;
 
